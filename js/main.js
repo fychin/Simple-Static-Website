a94032d207aa94afcb4c00225ec4c0778d5098f0
Yang Chin <fyang96c@gmail.com>
2017-02-06 13:26:31 -0800

    Add jQuery ScrollToElement for navbar btn, add github project link

--- js/main.js
+++ js/main.js
@@ -12,6 +12,44 @@ $(document).ready(function(){
 	// Init Wow.js
 	new WOW().init();
 
+	// Navbar menu scroll to section.
+	$('#nav-abt').click(function(){
+		$('html, body').animate({
+			scrollTop: $('#home').offset().top
+		}, 800);
+	});
+	$('#nav-edu').click(function(){
+		$('html, body').animate({
+			scrollTop: $('#education').offset().top
+		}, 800);
+	});
+	$('#nav-coursework').click(function(){
+		$('html, body').animate({
+			scrollTop: $('#coursework').offset().top
+		}, 800);
+	});
+	$('#nav-exp').click(function(){
+		$('html, body').animate({
+			scrollTop: $('#experience').offset().top
+		}, 800);
+	});
+	$('#nav-project').click(function(){
+		$('html, body').animate({
+			scrollTop: $('#project').offset().top
+		}, 800);
+	});
+	$('#nav-skills').click(function(){
+		$('html, body').animate({
+			scrollTop: $('#skills').offset().top
+		}, 800);
+	});
+	$('#nav-contact').click(function(){
+		$('html, body').animate({
+			scrollTop: $('#contact').offset().top
+		}, 800);
+	});
+
+
 	$(window).scroll(function() {
 		// Show scroll-top button if window is scrolled down.
 		if ($(this).scrollTop() > 100) {
